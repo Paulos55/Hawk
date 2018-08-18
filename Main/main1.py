7ad773f816189c3e66edfb7799d55d257344c6dd
tekkay <wellingtontekay@gmail.com>
2018-08-18 14:47:30 -0300

    Interface

--- Main/main1.py
+++ Main/main1.py
@@ -63,39 +63,7 @@ def search_web(text):
 			webbrowser.open_new_tab(url)
 			break		
 	return 'pesquisando'
-	return result
-	
-	
-def facebook_search(text):
-	resultf = None
-	resultM = None
-	
-	if text is not None:
-		for key in facebook_keywords:
-			if text.startswith('facebook'):
-				resultf = text.replace('facebook', '')
-				
-	if resultf is not None:
-		for url in search(text, stop=3):
-			browser = webdriver.Firefox()
-			browser.get('https://www.facebook.com/public/' + resultf)
-			btn = browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[2]/div/div[2]/div/div/div/div[2]/div[1]/div[1]/div/div/div/a/img')
-			btn.click()
-			break
-	return 'Encontrado'
-	return resultf
-	
-	if text.startswith('mensagem'):
-		resultM = text.replace('mensagem', '')
-		btn = browser.find_element_by_xpath('//*[@id="u_0_1f"]')
-		btn.click()
-		pyautogui.typewrite(resultM)
-	if text.startswith('enviar'):
-		pyautogui.press('enter')
-					
-	return 'Pode dizer a sua mensagen'
-	return resultM
-				
+	return result + rstrip()
 
 def speak(text):
     speaker.say(text)
